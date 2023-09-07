be4795119fa21d12417a9aee256137f3b0ce8404
Aguicarpy <aguicarpy@gmail.com>
2023-09-07 16:59:34 -0400

    feature GET por id, put y delete

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3015
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     server.listen(port, () => {
       console.log(`Escuchando en el puerto ${port}`); // eslint-disable-line no-console
     });
